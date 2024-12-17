48d2e14f3422529fa706567ba854feae3128581f
Moedrian <ikamitse@gmail.com>
2024-12-17 21:14:03 +0800

    add tomatoNow button and adjust layout

--- src/Tomato/MainWindow.xaml.cs
+++ src/Tomato/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ public partial class MainWindow : Window
 {
     private NotifyIcon? _icon;
 
-    private readonly string[] _intervals = new[] { 5, 10, 15, 20, 25, 30, 35, 40, 45 }.Select(i => i.ToString()).ToArray();
+    private readonly string[] _intervals = Enumerable.Range(1, 9).Select(i => (i * 5).ToString()).ToArray();
     private readonly string[] _hours = Enumerable.Range(0, 24).Select(x => x.ToString("00")).ToArray();
     private readonly string[] _minutes = Enumerable.Range(0, 60).Select(x => x.ToString("00")).ToArray();
 
@@ -25,6 +25,8 @@ public partial class MainWindow : Window
 
     private const string DismissButtonArgKey = "action";
     private const string DismissButtonArgValue = "start";
+
+    private bool _toastOn;
 
     public MainWindow()
     {
@@ -110,7 +112,7 @@ public partial class MainWindow : Window
             {
                 ShowInTaskbar = false;
                 const string title = "Tomato Minimized";
-                const string text = "Click the tray icon to the config window.";
+                const string text = "Double click the tray icon to the config window.";
                 _icon?.ShowBalloonTip(0, title, text, ToolTipIcon.Info);
             }
             else if (WindowState == WindowState.Normal)
@@ -133,9 +135,20 @@ public partial class MainWindow : Window
                     {
                         Reset();
                         _timerCtrDwn?.Start();
+                        _toastOn = false;
                     });
                 }
             }
+        };
+
+        TomatoNowButton.Click += delegate
+        {
+            _ctrDwnInterval = TimeSpan.Zero;
+            CounterDown.Text = GetCtrDwnString();
+            _timerCtrDwn?.Stop();
+
+            if (!_toastOn)
+                DisplayTomatoNotification();
         };
 
         ApplyButton.Click += delegate
@@ -164,36 +177,41 @@ public partial class MainWindow : Window
     {
         var cfg = TomatoConfig.Deserialize();
 
-        IntervalBox.SelectedIndex = cfg.Interval / 5 - 1;
+        //IntervalBox.SelectedIndex = cfg.Interval / 5 - 1;
         HourBox.SelectedIndex = cfg.OffTimeHour;
         MinuteBox.SelectedIndex = cfg.OffTimeMinute;
 
         _ctrDwnInterval = TimeSpan.FromMinutes(cfg.Interval);
     }
 
-    private void DisplayTomatoNotification()
+    public static ToastContentBuilder GetToastContentBuilder()
     {
         const string title = "Tomato Clock Is Here.";
         const string text = "Get up to drink some water!";
         var msg = text + Environment.NewLine + CalculateOff();
 
-        Dispatcher.Invoke(delegate
-        {
-            var builder = new ToastContentBuilder()
-                .AddText(title)
-                .AddText(msg)
-                .SetToastScenario(ToastScenario.Reminder)
-                .AddButton(new ToastButton()
-                    .SetContent("Dismiss")
-                    .AddArgument(DismissButtonArgKey, DismissButtonArgValue)
-                    .SetBackgroundActivation());
+        var builder = new ToastContentBuilder()
+            .AddText(title)
+            .AddText(msg)
+            .SetToastScenario(ToastScenario.Reminder)
+            .AddButton(new ToastButton()
+                .SetContent("Start Another Tomato")
+                .AddArgument(DismissButtonArgKey, DismissButtonArgValue)
+                .SetBackgroundActivation());
 
-            if (File.Exists(TomatoConfig.GetTomatoPicture()))
-                builder.AddAppLogoOverride(new Uri("file:///" + TomatoConfig.GetTomatoPicture()),
-                    ToastGenericAppLogoCrop.Circle);
+        if (File.Exists(TomatoConfig.GetTomatoPicture()))
+            builder.AddAppLogoOverride(new Uri("file:///" + TomatoConfig.GetTomatoPicture()),
+                ToastGenericAppLogoCrop.Circle);
 
-            builder.Show();
-        });
+        return builder;
+    }
+
+    private void DisplayTomatoNotification()
+    {
+        if (!_toastOn)
+            GetToastContentBuilder().Show();
+
+        _toastOn = true;
     }
 
     private void Start()
@@ -230,13 +248,13 @@ public partial class MainWindow : Window
     {
         try
         {
-            var interval = int.Parse(IntervalBox.SelectedItem as string ?? throw new Exception("Interval parsing error."));
+            //var interval = int.Parse(IntervalBox.SelectedItem as string ?? throw new Exception("Interval parsing error."));
             var hour = int.Parse(HourBox.SelectedItem as string ?? throw new Exception("Hour parsing error."));
             var minute = int.Parse(MinuteBox.SelectedItem as string ?? throw new Exception("Minute parsing error."));
 
             var cfg = new TomatoConfig
             {
-                Interval = interval,
+                //Interval = interval,
                 OffTimeHour = hour,
                 OffTimeMinute = minute
             };
